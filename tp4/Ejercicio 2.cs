8f7a042bad3f096e6aa71cfac1144ddacc9e71a6
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-10-27 19:33:54 -0300

    Update and rename Ejercicio 2 to Ejercicio 2.cs

--- tp4/Ejercicio 2.cs
+++ tp4/Ejercicio 2.cs
@@ -1,3 +1,9 @@
+/** 2. Se desea modelar el funcionamiento de un banco en el cual existen 5 cajas para realizar
+pagos. Existen P clientes que desean hacer un pago. Para esto, cada una selecciona la caja
+donde hay menos personas esperando; una vez seleccionada, espera a ser atendido. En cada
+caja, los clientes son atendidos por orden de llegada por los cajeros. Luego del pago, se les
+entrega un comprobante. Nota: maximizar la concurrencia. **/
+
 chan llegada[5](int);
 chan esperaTurno[P](int);
 chan pagar[5](Pago);
