1950000fe4337fbd7b6905e880c0ee2885e43c33
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-10-27 20:02:06 -0300

    Create Ejercicio 5b.cs

---
+++ tp4/Ejercicio 5b.cs
@@ -0,0 +1,35 @@
+/** b) Modifique la solución implementada para que considere la presencia de un director de
+oficina que también usa las impresoras, el cual tiene prioridad sobre los administrativos. **/
+
+chan colaImpresion(texto); //Administrativos envían documentos a esta cola de impresión
+chan colaImpresionPrioritaria(texto); //Director envía sus documentos a esta cola prioritaria
+
+Process Administrativo[id:0..N-1] {
+    texto doc;
+    while(true) {
+        doc = generarDoc();
+        send(colaImpresion(doc)); //Envía documento a imprimir
+    }
+}
+
+Process Impresora[id:0..2] {
+    texto doc;
+    while(true) {
+        if(empty(colaImpresionPrioritaria)) ->
+            if(not empty(colaImpresion)) {
+                receive(colaImpresion(doc)); //Si hay un doc en la cola no prioritaria, lo recibe
+                imprimirDocumento(doc); //Imprime
+            }
+        [] (not empty(colaImpresionPrioritaria)) ->
+            receive(colaImpresionPrioritaria(doc)); //Si hay un doc en la cola prioritaria, lo recibe
+            imprimirDocumento(doc); //Imprime
+    }
+}
+
+Process Director {
+    texto doc;
+    while(true) {
+        doc = generarDoc();
+        send(colaImpresionPrioritaria(doc));
+    }
+}
