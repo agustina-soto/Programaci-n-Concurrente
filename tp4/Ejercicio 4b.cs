b970de42832ec0e411069065a188953e7e3353ca
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-10-27 19:36:04 -0300

    Update and rename Ejercicio 4b to Ejercicio 4b.cs

--- tp4/Ejercicio 4b.cs
+++ tp4/Ejercicio 4b.cs
@@ -1,4 +1,15 @@
-b) Con prioridad (los clientes que ya usaron la cabina y quieren pagar tienen prioridad ante aquellos clientes que recien llegaron y necesitan saber a qué cabina dirigirse).
+/** 4. Simular la atención en un locutorio con 10 cabinas telefónicas, el cual tiene un empleado
+que se encarga de atender a N clientes. Al llegar, cada cliente espera hasta que el empleado
+le indique a qué cabina ir, la usa y luego se dirige al empleado para pagarle. El empleado
+atiende a los clientes en el orden en que hacen los pedidos. A cada cliente se le entrega un
+ticket factura por la operación.
+
+b) Modifique la solución implementada para que el empleado dé prioridad a los que
+terminaron de usar la cabina sobre los que están esperando para usarla.
+Nota: maximizar la concurrencia; suponga que hay una función Cobrar() llamada por el
+empleado que simula que el empleado le cobra al cliente.
+
+Nota: Los clientes que ya usaron la cabina y quieren pagar tienen prioridad ante aquellos clientes que recien llegaron y necesitan saber a qué cabina dirigirse. **/
 
 
 chan llegada(int); //Clientes avisan que llegaron
