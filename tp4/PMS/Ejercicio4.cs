d3d49f80cd339c912d76bbf66cb936cf5cd48326
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-10-28 16:05:09 -0300

    Create Ejercicio4.cs

---
+++ tp4/PMS/Ejercicio4.cs
@@ -0,0 +1,8 @@
+/** En una exposición aeronáutica hay un simulador de vuelo (que debe ser usado con exclusión mutua)
+y un empleado encargado de administrar su uso. Hay P personas que esperan a que el empleado lo deje
+acceder al simulador, lo usa por un rato y se retira.
+  a) Implemente una solución donde el empleado sólo se ocupa de garantizar la exclusión mutua.
+  b) Modifique la solución anterior para que el empleado considere el orden de llegada para dar
+     acceso al simulador. Nota: cada persona usa sólo una vez el simulador. **/
+
+
