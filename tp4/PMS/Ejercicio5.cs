30cc282e1f6c0d77a4977e0e2c7cc34a47b91988
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-10-28 16:05:55 -0300

    Create Ejercicio5.cs

---
+++ tp4/PMS/Ejercicio5.cs
@@ -0,0 +1,6 @@
+/* En un estadio de fútbol hay una máquina expendedora de gaseosas que debe ser usada porE Espectadores
+de acuerdo con el orden de llegada. Cuando el espectador accede a la máquina en su turno usa la máquina
+y luego se retira para dejar al siguiente.
+
+Nota: cada Espectador una sólo una vez la máquina. */
+
