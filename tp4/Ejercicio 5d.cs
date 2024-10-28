b70fc7dee3d8ebedb8fe3c1af7191d4d012696ca
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-10-27 22:35:38 -0300

    Update Ejercicio 5d.cs

--- tp4/Ejercicio 5d.cs
+++ tp4/Ejercicio 5d.cs
@@ -2,40 +2,66 @@
 imprimen 10 trabajos y que todos los procesos deben terminar su ejecución.
 Nota: ni los administrativos ni el director deben esperar a que se imprima el documento. **/
 
-chan colaImpresion(texto); //Administrativos envían documentos a esta cola de impresión
-chan colaImpresionPrioritaria(texto); //Director envía sus documentos a esta cola prioritaria
+chan pedidos(texto); //Administrativos envían documentos a esta cola de impresión
+chan pedidosConPrioridad(texto); //Director envía sus documentos a esta cola prioritaria
+chan colaImpresora[3](texto); //Cola privada de cada Impresora
 
 Process Administrativo[id:0..N-1] {
-    texto doc; int cant_docs = 0;
-    while(cant_docs < 10) {
+    int i; texto doc;
+    for i in 1..10 { //Imprime 10 documentos
         doc = generarDoc();
-        send(colaImpresion(doc)); //Envía documento a imprimir
-        cant_docs++;
-    }
-}
-
-Process Impresora[id:0..2] {
-    texto doc; int cant_impresiones = 0, limite = N*10 + 10;
-    while(cant_impresiones < limite) {
-        //BW
-        if(empty(colaImpresionPrioritaria)) ->
-            if(not empty(colaImpresion)) {
-                receive(colaImpresion(doc)); //Si hay un doc en la cola no prioritaria, lo recibe
-                imprimirDocumento(doc); //Imprime
-                cant_impresiones++; //Incrementa cantidad de documentos impresos
-            }
-        [] (not empty(colaImpresionPrioritaria)) ->
-            receive(colaImpresionPrioritaria(doc)); //Si hay un doc en la cola prioritaria, lo recibe
-            imprimirDocumento(doc); //Imprime
-            cant_impresiones++; //Incrementa cantidad de documentos impresos
+        send(pedido(doc)); //Envía documento a imprimir
     }
 }
 
 Process Director {
-    texto doc; int cant_docs = 0;
-    while(cant_docs < 10) {
+    int i; texto doc;
+    for i in 1..10 { //Imprime 10 documentos
         doc = generarDoc();
-        send(colaImpresionPrioritaria(doc));
-        cant_docs++;
+        send(pedido(doc)); //Envía documento a imprimir
+    }
+}
+
+Process Impresora[id:0..2] {
+    texto doc; boolean continuar = true;
+    while(continuar) {
+        receive(colaImpresora[id](doc)); //Espera a que el Coordinador le envíe un documento a imprimir
+        if(doc <> "FIN") -> imprimirDocumento(doc); //Si es válido lo imprime, sino corta la ejecución del proceso
+        [] (doc.equals("FIN")) continuar = false;
+    }
+}
+
+Process Coordinador {
+    int i, id; texto doc; int contador = 0;
+
+    while (contador < N*10+10) { //Sigue siendo BW esto no? :/ no se me ocurre cómo mejorarlo
+        
+        if(not empty(pedidosConPrioridad)) ->
+            receive(pedidoConPrioridad(doc)); //Recibe el pedido prioritario
+            
+            //Envía pedido a alguna impresora (aleatoria entre las disponibles)
+            if(empty(colaImpresora[0])) -> send(colaImpresora[0](doc));
+            [] if(empty(colaImpresora[1])) -> send(colaImpresora[1](doc));
+            [] if(empty(colaImpresora[2])) -> send(colaImpresora[2](doc));
+            end if
+
+            contador++;
+
+        [] (empty(pedidosConPrioridad) and (not empty(pedidos)))-> //Si no tengo pedidos con prioridad pero tengo pedidos normales
+            receive(pedido(doc)); //Recibe el pedido normal
+
+            //Envía pedido a alguna impresora (aleatoria entre las disponibles)
+            if(empty(colaImpresora[0])) -> send(colaImpresora[0](doc));
+            [] if(empty(colaImpresora[1])) -> send(colaImpresora[1](doc));
+            [] if(empty(colaImpresora[2])) -> send(colaImpresora[2](doc));
+            end if
+
+            contador++;
+        
+        endif
+    }
+
+    for i in 0..2 {
+        send(colaImpresora[i]("FIN"));
     }
 }
