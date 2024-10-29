9f65e62af7fd85fea0bd5dd6954921a3c7886e87
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-10-29 00:42:56 -0300

    Update Ejercicio5.cs

--- tp4/PMS/Ejercicio5.cs
+++ tp4/PMS/Ejercicio5.cs
@@ -6,8 +6,8 @@ Nota: cada Espectador una sólo una vez la máquina. */
 
 Process Espectadores [id:0..E-1] {
     Admin!llegada(id); //Avisa que llegó
-    Admin?miTurno(); //Espera a que le avisen que es su turno de usar la máquina
-    usarMaquina();
+    Admin?usarMaquina(); //Espera a que le avisen que es su turno de usar la máquina
+    usandoMaquinaExpendedora();
     Admin!fin(); //Avisa que terminó de usar la máquina
 }
 
@@ -19,14 +19,14 @@ Process Buffer {
             push(buffer(idE)); //Agrega el id del espectador a la fila
         [] (maquinaLibre) -> //Si la máquina está disponible para usar
             maquinaLibre = false; //Actualiza el estado de la máquina a "ocupada"
-            Espectador[idE]!miTurno(); //Avisa al espectador que es su turno
+            Espectador[idE]!usarMaquina(); //Avisa al espectador que es su turno
             cantP++;
         end if
     [] Espectador[*]?fin() -> //Si alguien avisa que terminó de usar la máquina
         if(empty (buffer)) -> //Si no hay más espectadores en la fila
             maquinaLibre = true; //Actualiza el estado de la máquina a "disponible"
         [] (not empty(buffer)) -> //Si quedan espectadores en la fila, tiene que pasar el que está primero
-            Espectador[pop(buffer)]!miTurno(); //Avisa al espectador que se recuperó de la cola que es su turno
+            Espectador[pop(buffer)]!usarMaquina(); //Avisa al espectador que se recuperó de la cola que es su turno
         end if
     end if
 }
