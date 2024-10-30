38ad55e689292c30d1907e45b45f597f9e7e56d1
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2024-10-30 13:23:22 -0300

    Update Ejercicio5.cs

--- tp4/PMS/Ejercicio5.cs
+++ tp4/PMS/Ejercicio5.cs
@@ -4,7 +4,7 @@ y luego se retira para dejar al siguiente.
 
 Nota: cada Espectador una sólo una vez la máquina. */
 
-Process Espectadores [id:0..E-1] {
+Process Espectador [id:0..E-1] {
     Admin!llegada(id); //Avisa que llegó
     Admin?usarMaquina(); //Espera a que le avisen que es su turno de usar la máquina
     usandoMaquinaExpendedora();
@@ -16,7 +16,7 @@ Process Admin {
 
     do (cantP < E); Espectador[*]?llegada(idE) -> //Espera para recibir el id de un nuevo espectador
         if(!maquinaLibre) -> //Si la máquina no está disponible para usar
-            push(buffer(idE)); //Agrega el id del espectador a la fila
+            push(buffer, idE); //Agrega el id del espectador a la fila
         [] (maquinaLibre) -> //Si la máquina está disponible para usar
             maquinaLibre = false; //Actualiza el estado de la máquina a "ocupada"
             Espectador[idE]!usarMaquina(); //Avisa al espectador que es su turno
@@ -29,4 +29,32 @@ Process Admin {
             Espectador[pop(buffer)]!usarMaquina(); //Avisa al espectador que se recuperó de la cola que es su turno
         end if
     end if
+}
+
+// PREGUNTAR SI PUEDO USAR 3 PROCESOS (ESPECTADOR-ADMIN-MAQUINA) --> Si se permite lo puedo resolver de la misma forma de siempre
+// Hecho con 3 procesos:
+
+Process Espectador [id:0..E-1] {
+    Admin!llegada(id); //Avisa que llegó
+    Maquina?usarMaquina(); //Espera a que le avisen que es su turno de usar la máquina
+    usandoMaquinaExpendedora();
+    Maquina!fin(); //Avisa que terminó de usar la máquina
+}
+
+Process Admin {
+    int idE; cola buffer;
+    do Espectador[*]?llegada(idE) -> //Recibe un espectador
+        push(buffer, id); //Agrega el id a la fila
+    [] not empty(buffer) ; Maquina?disponible -> //Si hay espectadores en la fila y la máquina está disponible
+        Maquina!siguiente(pop(buffer)); //Envía a la máquina quién es el siguiente en la fila
+}
+
+Process Maquina {
+    int i, idPersona;
+    for i in 1..E {
+        Admin!disponible(); //Avisa que está disponible
+        Admin?siguiente(idPersona); //Espera a saber quién sigue
+        Espectador[idPersona]!usarMaquina(); //Avisa a una persona que es su turno de usar la máquina
+        Espectador[idPersona]?fin(); //Espera a que la persona termine de usar la máquina
+    }
 }
